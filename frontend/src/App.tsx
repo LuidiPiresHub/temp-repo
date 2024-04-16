f976a85303cb10212178d5d51ee94e6059fc52d5
LuidiPires <luidihot@gmail.com>
2024-04-16 03:17:08 -0300

    fix: variavel de ambiente errada

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import axios, { AxiosError } from 'axios';
 
 export default function App() {
   const api = axios.create({
-    baseURL: import.meta.env.API_URL || 'http://localhost:3001',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001',
     withCredentials: true,
     headers: {
       'Content-Type': 'application/json',
